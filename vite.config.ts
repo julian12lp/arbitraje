4420a738f8ac941eed9f2be43cb4db851de641f7
julian12lp <julian12lp@gmail.com>
2025-10-03 14:46:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // GitHub Pages necesita el subpath del repo.
 export default defineConfig({
   plugins: [react()],
-  base: '/arbitraje-usdt-ars/', // ⚠️ cambialo si el repo tiene otro nombre
+  base: '/arbitraje/', // ⚠️ cambialo si el repo tiene otro nombre
 })
